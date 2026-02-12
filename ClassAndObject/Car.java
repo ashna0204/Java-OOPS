79b75887137336ac7ab3cbf46e03f19f1ddca406
ashna <ashna@midhaan.com>
2026-02-12 12:53:40 +0530

    class and object

---
+++ ClassAndObject/Car.java
@@ -0,0 +1,16 @@
+package ClassAndObject;
+
+public class Car {
+    
+    String brand;
+    int year;
+
+    public Car(String brand, int year){
+        this.brand = brand;
+        this.year  = year;
+    }
+
+    public void drive(){
+        System.out.println(brand + " is driving");
+    }
+}
---
+++ ClassAndObject/Main.java
@@ -0,0 +1,9 @@
+package ClassAndObject;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Car car1 = new Car("BMW", 2000);
+        car1.drive();
+    }
+}

e3c6d9f615ddca9dfa168266e7c4caccd3b37809
ashna <ashna@midhaan.com>
2026-02-12 17:14:01 +0530

    method overloading

---
+++ MethodOverloading/Main.java
@@ -0,0 +1,10 @@
+package MethodOverloading;
+
+public class Main {
+    
+    public static void main(String[] args){
+        MathUtils mu = new MathUtils();
+        System.out.println(mu.add(3,2));
+        System.out.println(mu.add(5.2,3.2));
+    }
+}
